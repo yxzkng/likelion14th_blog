48a9007c4036651cae5cd74813e99671ae5eeaf2
yxzkng <yunjin4809@naver.com>
2026-05-06 13:11:23 +0900

    feat: initial commit

---
+++ src/test/java/likelion14th/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion14th.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
